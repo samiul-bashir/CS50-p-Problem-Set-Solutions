a47139f053eb8948a874b61f08a0a1113ab0fb10
samiul-bashir <samiulbashir786@gmail.com>
2024-07-02 01:27:24 +0530

    Create Fuel Gauge.py
    In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer, and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. And if 99% or more remains, output F instead to indicate that the tank is essentially full.
    
    If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.

---
+++ Fuel Gauge.py
@@ -0,0 +1,22 @@
+def main():
+    x=fraction("Fraction:")
+    print(x)
+
+def fraction(prompt):
+    while True:
+        try:
+            neu,deno=input(prompt).split("/")
+            value=((int(neu)/int(deno))*100)
+            if value<=1:
+                return "E"
+            elif 90<= value<=100:
+                return "F"
+            elif value>100:
+                pass
+            else:
+                return str(int(round(value)))+"%"
+        except ZeroDivisionError:
+            pass
+        except ValueError:
+            pass
+main()
